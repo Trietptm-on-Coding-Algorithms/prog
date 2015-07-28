eb9f97cef18e0c693969e9dd086d5c98d9c61d67
lebarton <bartonleland@gmail.com>
2015-07-27 19:22:00 -0500

    Start C++ Cleanup

--- CPP/CPP_Features/src/DataStructures/Enumerated.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-
-int main() {
-	enum color { red, green=5, blue} c1;
-	c1 = blue; //=6
-	c1 = green;
-}
--- CPP/CPP_Features/src/Functional/Functions.cpp
+++ CPP/CPP_Features/src/Functional/Functions.cpp
@@ -9,8 +9,3 @@ int addThree( int int1 = 1, int int2 = 2, int int3 = 3 ) {
 double addThree( long long1 = 1.1, long long2 = 2.2, long long3 = 3.3) {
 	return long1 + long2 + long3;
 }
-
-int main() 
-{
-
-}
