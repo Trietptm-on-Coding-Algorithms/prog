3c4d146aacaae3540f82411ab46066dbc6292fa0
Leland <bartonleland@gmail.comm>
2017-02-16 23:58:43 -0500

    improvements

--- cpp/cpp_features/src/control_flow.cpp
+++ cpp/cpp_features/src/control_flow.cpp
@@ -1,20 +1,12 @@
 #include <iostream>
 
 
-int addThree( int int1 = 1, int int2 = 2, int int3 = 3 ) {
-	//all paramters are local variables
-		//strict pass by value unless you are passing address
-	return int1+int2+int3;
-}
-
-double addThree( double long1 = 1.1, double long2 = 2.2, double long3 = 3.3) {
-	return long1 + long2 + long3;
-}
 
 namespace controlflow {
 
 	void demo() {
-		std::cout << "This is the control flow" << std::endl;
+		//C++ doesn't have any control flow changes from C
+		//If..else, a case statement are basics
 	}
 
 }
--- cpp/cpp_features/src/data_types.cpp
+++ cpp/cpp_features/src/data_types.cpp
@@ -20,34 +20,10 @@ namespace cpp_demo {
 	void data_types_demo() {
 
 	
-		//basic data types
-		unsigned short int usi = 65535;
-		short int si = 32767; //-32768
-		unsigned long int uli = 4294967295;
-		long int li = 2147483648; // - of number -1
-		char c = 'a';
+		//basic data types are included from C
+		//
+		//Bool is included by default
 		bool b = true;
-		float f = 1.2e-38; //to 3.4e38
-		
-
-		int a, ab, abc;
-		
-		//symbolic
-		a = symboliccons;
-		std::cout << a << std::endl;
-
-		b = usi >= si;
-		b = si == si;
-		b = si != usi;
-		b = true;
-		
-		if ( b || b ) 
-			std::cout << "Conditional\n";
-		
-		if ( !b && b )
-			std::cout << "Cond\n";
-		else
-			std::cout << "Else\n";
 
 	}
 
